266fe22930fbe761ed90e9d98873958e4326f804
ErykSikora <eryksikora95@gmail.com>
2020-07-24 15:06:36 +0200

    read file included

--- sheets.php
+++ sheets.php
@@ -1,28 +1,52 @@
 <?php
 
-    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';                               // include mechanizmu [composer]
 
-    echo ';test';
+    # M A I N    C O N F I G
 
-    // $client = new \Google_Client();
-    // $client->setApplicationName('Excel z danymi');
-    // $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);
-    // $client->setAccessType('offline');
-    // $client->setAuthConfig(_DIR_ . '/credentials.json');
-    // $service = new Google_Service_Sheet($client);
+    $client = new \Google_Client();                                         // deklaracja klienta
+    $client->setApplicationName('Excel z danymi');                          // tytuł aplikacji (informacja tylko na nasz użytek)
+    $client->setScopes([\Google_Service_Sheets::SPREADSHEETS]);             // definiujemy zakresy, które ma obsługiwać nasze naszędzie
+    $client->setAccessType('offline');                                      // domyślnie, polecam tak zostawić
+    $client->setDeveloperKey("**");    // klucz API
+    $client->setAuthConfig(__DIR__ . '/credentials.json');                  // credentials.json generowany z Google API (musi dostarczyć klient - właściciel konta google)
 
-    // $spreadsheetId = "1XE1QZR0MNK-RIh8Unn3ite4IVJB0nb10gPQGs0jKayc";
+    $service = new Google_Service_Sheets($client);                          // deklaracja połączenia
 
-    // $range = "Sheet1!A2:J14";
-    // $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-    // $values = $response->getValues();
-    // if empty($values){
+    $spreadsheetId = "**";        // id naszego pliku (do wydobycia z linka dokumentu)
+
+    $range = "Sheet1!A15:B15";                                               // zakres odczytywanych plików 
+    // $response = $service->spreadsheets_values->get($spreadsheetId, $range); // żądanie pobrania informacji z Google
+    // $values = $response->getValues();                                       // pobieramy odpowiedź
+
+    // if (empty($values)){
     //     print "No data found";
     // } else {
     //     $mask = "%10s %-10s %s\n";
     //     foreach ($values as $row) {
-    //         echo sprintf($mask, $row[2], $row[1], $row[0]);
+    //         print_r($row);
     //     }
     // }
+
+    $values = [
+        ["Snow John", "umarl@przezyl.com"],
+        ["Bravo Johnny", "XD@yahoo.com"],
+        ["Mendes Eva", "em@test.com"]
+    ];
+
+    $body = new Google_Service_Sheets_ValueRange([
+        'values' => $values
+    ]);
+
+    $params = [
+        'valueInputOption' => 'RAW'
+    ];
+
+    $result = $service->spreadsheets_values->update(
+        $spreadsheetId,
+        $range,
+        $body,
+        $params
+    );
 
 ?>
